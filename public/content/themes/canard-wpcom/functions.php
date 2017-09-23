4307088fa48836591e939edbe77269452ade6264
Peter Hoang <peter.hoang@ansarada.com>
2017-09-23 16:23:26 +0700

    Add header widget

--- public/content/themes/canard-wpcom/functions.php
+++ public/content/themes/canard-wpcom/functions.php
@@ -270,3 +270,19 @@ require get_template_directory() . '/inc/jetpack.php';
 // updater for WordPress.com themes
 if ( is_admin() )
 	include dirname( __FILE__ ) . '/inc/updater.php';
+
+// define header widget
+function wpb_widgets_init() {
+	
+	   register_sidebar( array(
+		   'name'          => 'Custom Header Widget Area',
+		   'id'            => 'custom-header-widget',
+		   'before_widget' => '<div class="chw-widget">',
+		   'after_widget'  => '</div>',
+		   'before_title'  => '<h2 class="chw-title">',
+		   'after_title'   => '</h2>',
+	   ) );
+	
+   }
+   add_action( 'widgets_init', 'wpb_widgets_init' );
+   
--- public/content/themes/canard-wpcom/header.php
+++ public/content/themes/canard-wpcom/header.php
@@ -82,3 +82,10 @@
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+
+	<?php
+	if ( is_active_sidebar( 'custom-header-widget' ) ) : ?>
+		<div id="header-widget-area" class="chw-widget-area widget-area" role="complementary">
+		<?php dynamic_sidebar( 'custom-header-widget' ); ?>
+		</div>
+	<?php endif; ?>
